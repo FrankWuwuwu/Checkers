c554834753690fef7d98dffeee37e8ba3f13c31b
Kefu Wu <524663104@qq.com>
2021-12-18 08:22:51 -0500

    dataset

--- data_generator.py
+++ data_generator.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     board_list=[]
     utility_list=[]
     
-    node=node_tree(state,1000)
+    node=node_tree(state,10000)
     get_child(node)
     
     # board_list, 10*10 list, need to change to 1*10*10
